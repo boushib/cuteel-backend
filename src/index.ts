6d6f846b46dd2d27f772e5a08201d9b4d1b8c1f0
boushib <boushib01@gmail.com>
2021-07-17 00:08:11 +0100

    connect to mongo db

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from 'express'
 import { config } from 'dotenv'
+import { MongoClient } from 'mongodb'
 
 config()
 
 const app = express()
+
+MongoClient.connect(process.env.DATABASE_URL!, (error, client) => {
+  if (error) throw error
+  const db = client?.db('cuteel')
+  db?.collection('test')
+    .find()
+    .toArray((error, res) => {
+      if (error) throw error
+      console.log(res)
+    })
+})
 
 app.get('/', (req, res) => {
   res.json({ data: 'hello' })
