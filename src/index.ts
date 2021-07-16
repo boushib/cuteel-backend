b61c2a5ef95f641442ad5e46c6043102cb90f9cc
boushib <boushib01@gmail.com>
2021-07-16 22:51:00 +0100

    setup a basic express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import express from 'express'
 
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ data: 'hello' })
+})
+
+const PORT = 8080
+
+app.listen(PORT, () => {
+  console.log(`App started at port ${PORT}`)
+})
