586c8c7f2fe6ae03201702975c55442cc147721a
boushib <boushib01@gmail.com>
2021-07-17 14:54:04 +0100

    display a user friendly message

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,12 @@
 import User from '../models/user'
 import { Request, Response } from 'express'
+import { formatMongoError } from '../utils'
 
 export const signup = (req: Request, res: Response) => {
   const user = new User(req.body)
   user.save((error: any, user: any) => {
     if (error) {
-      return res.status(400).json({ error })
+      return res.status(400).json({ error: formatMongoError(error) })
     }
     res.status(201).json({ user })
   })
---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export const formatMongoError = (error: any) => {
+  if (error.code === 11000) {
+    const field = Object.keys(error.keyPattern)[0]
+    return `An account with this ${field} already exists!`
+  }
+  return 'Error saving data to database'
+}
